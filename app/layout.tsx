eaec5c9c02c703c0b5df69a71179f026d0140039
IEL4422 <contact@illinoisestatelaw.com>
2026-04-20 12:42:34 -0500

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,11 +12,11 @@ export const metadata: Metadata = {
   },
   generator: 'WordPress 6.9',
   icons: {
-    icon: 'https://i.imgur.com/bSfExtA.png',
-    apple: 'https://i.imgur.com/bSfExtA.png',
+    icon: 'https://i.imgur.com/pKy65wF.png',
+    apple: 'https://i.imgur.com/pKy65wF.png',
   },
   other: {
-    'msapplication-TileImage': 'https://i.imgur.com/bSfExtA.png',
+    'msapplication-TileImage': 'https://i.imgur.com/pKy65wF.png',
   }
 };
 
